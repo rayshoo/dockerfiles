f810a9793d24670f366a84649152e518147cdacb
Eohyung Lee <liquidnuker@gmail.com>
2019-03-27 18:18:45 +0900

    Add winetricks file

--- kakao/entrypoint.sh
+++ kakao/entrypoint.sh
@@ -6,11 +6,8 @@ if [ ! -e "$HOME/.wine/drive_c/Program Files/Kakao/KakaoTalk/KakaoTalk.exe" ]; t
     echo "Install wineboot"
     WINEARCH=win32 WINEPREFIX=~/.wine wine wineboot
 
-    wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks \
-        && chmod +x ./winetricks \
-        && ./winetricks -q d3dx11_43 gdiplus richtx32 msxml6 \
-        && ./winetricks win7 \
-        && ./winetricks sound=pulse
+        winetricks -q d3dx11_43 gdiplus richtx32 msxml6 \
+        && winetricks win7
 
     wget http://app.pc.kakao.com/talk/win32/KakaoTalk_Setup.exe \
         && wine KakaoTalk_Setup.exe
